0cfe5a0bbeb1d84536c2225e70784c12cffde51c
MarcsVinicius <marcos.vgalencar@gmail.com>
2022-07-01 22:48:04 -0300

    fix: added app.use in errorMiddleware

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const errorMiddleware = require('./src/middlewares/errorMiddleware');
 const router = require('./src/routers/task');
 
 const app = express();
 
 app.use(express.json());
 app.use('/task', router);
+app.use(errorMiddleware);
 
 module.exports = app;
