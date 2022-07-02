499e04758b1255209938022da555c9590017b5fb
MarcsVinicius <marcos.vgalencar@gmail.com>
2022-07-01 22:50:18 -0300

    feat: taskControllers functions done

--- src/backend/src/controllers/taskControllers.js
+++ src/backend/src/controllers/taskControllers.js
@@ -1,9 +1,41 @@
 const taskService = require('../services/taskService');
 
-const getAll = async (_req, res, next) => {
+const getAll = async (req, res, next) => {
   try {
-    const service = await taskService.getAll();
+    const service = await taskService.getAll(req.query.order);
     res.status(200).json(service);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const newTask = async (req, res, next) => {
+  try {
+    const { task } = req.body;
+    const service = await taskService.newTask(task);
+    res.status(201).json(service);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const deleteTask = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await taskService.deleteTask(id);
+    res.status(204).end();
+  } catch (err) {
+    next(err);
+  }
+};
+
+const updateTask = async (req, res, next) => {
+  try {
+    const { type } = req.query;
+    const { id } = req.params;
+    const { content } = req.body;
+    await taskService.updateTask(parseInt(id, 10), content, type);
+    res.status(204).end();
   } catch (err) {
     next(err);
   }
@@ -11,4 +43,7 @@ const getAll = async (_req, res, next) => {
 
 module.exports = {
   getAll,
+  newTask,
+  deleteTask,
+  updateTask,
 };
