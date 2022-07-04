94e7c3006222b1e7c94459dfe5f6402fdaf88a2b
MarcsVinicius <marcos.vgalencar@gmail.com>
2022-07-04 16:22:50 -0300

    feat: added cors

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const errorMiddleware = require('./src/middlewares/errorMiddleware');
 const router = require('./src/routers/task');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/task', router);
 app.use(errorMiddleware);
